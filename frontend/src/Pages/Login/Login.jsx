294adb15c7d4b72c9da383a3d5531040776855a8
Lokesh838 <lokeswaroram27@gmail.com>
2025-07-24 12:13:05 +0530

    added from lokesh

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -3,7 +3,7 @@ function Login(){
 
     return(
         <>
-         <h1>create your new account</h1>
+         <h1>create your new accounts</h1>
         <form action="" className={styles.f}>
         <input type="text" placeholder="name" />
         <input type="text" placeholder="email" />
