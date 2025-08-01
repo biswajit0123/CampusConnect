4b27e1ea5fe945a5c7477d80f66a3ee6eec3d935
biswajit0123 <biswajitmuduli0544@gmail.com>
2025-08-01 10:13:42 +0530

    login desined

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -2,51 +2,33 @@ import React, { useState } from 'react';
 import styles from './Login.module.css';
 
 const Login = () => {
-  const [activeForm, setActiveForm] = useState('login');
-
-  const showForm = (formId) => {
-    setActiveForm(formId);
-  };
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    // TODO: Add actual login/auth logic here
-
-    // Redirect to homepage after login
-    navigate('/HomePage');
-  };
+ 
 
   return (
     <>
       
-
       <div className={styles.container}>
-      <div className={`${styles.formBox} ${activeForm === 'login' ? styles.active : ''}`} id="login-form">
-        <form>
-          <h2>Login</h2>
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <button type="submit" name="login">Login</button>
+         <h1>Login With Credentials</h1>
+
+         <form action="" className={styles.frm}>
+           
+           <div className={styles.feild}>
+           <input type="text" placeholder='example@gmail.com'/>
+           </div>
+
+           <div className={styles.feild}>
+              <input type="text" placeholder='******'/>
+           </div>
+           
+           <button type='button' className={styles.btn}>Login</button>
+         </form>
+
           <p>
-            Don't have any account?{' '}
-            <a href='/Signup' style={{ cursor: 'pointer' }}>Sign up</a>
+            Already have any account?
+            <a href='/login'>Login</a>
           </p>
-        </form>
       </div>
-      {/* <div className={`${styles.formBox} ${activeForm === 'signup' ? styles.active : ''}`} id="signup-form">
-        <form>
-          <h2>Register</h2>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <button type="submit" name="register">Register</button>
-          <p>
-            Already have any account?{' '}
-            <a onClick={() => showForm('login')} style={{ cursor: 'pointer' }}>Login</a>
-          </p>
-        </form>
-      </div> */}
-    </div>
+
     </>
   );
 };
